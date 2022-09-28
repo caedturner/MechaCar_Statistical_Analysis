b0a70b2c2b0cf5c41b4b8e227e069b9c278f5aec
caedturner <106132054+caedturner@users.noreply.github.com>
2022-09-28 16:08:50 -0700

    Add Finalized
    Add Finalized

--- MechaCarChallenge.Rscript.R
+++ MechaCarChallenge.Rscript.R
@@ -11,3 +11,9 @@ total_summary <- sus_coil %>%
 # Create the Summary By Lot
 lot_summary <- sus_coil %>% group_by(Manufacturing_Lot) %>% 
   summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep')
+# Deliverable 3
+all_lots <- t.test(sus_coil$PSI,mu=1500)
+# For each specific lot
+lot_1 <- t.test(subset(sus_coil,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
+lot_2 <- t.test(subset(sus_coil,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
+lot_3 <- t.test(subset(sus_coil,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
